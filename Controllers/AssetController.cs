dde5eb28230c53874eae28af61dd0c23f4b1b61b
Jon Emil Sørensen <emil@cogoapp.io>
2024-07-03 01:21:47 +0200

    Fixed renamed AssetService references

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -26,7 +26,7 @@ namespace DACM.Controllers
 		[SwaggerResponse(200, "List of assets", typeof(List<Asset>))]
 		public async Task<IActionResult> List()
 		{
-			var assetMetadata = await _contentService.GetAssetAsync();
+			var assetMetadata = await _contentService.List();
 			if (assetMetadata == null)
 			{
 				return NotFound();
@@ -42,7 +42,7 @@ namespace DACM.Controllers
 		[SwaggerResponse(200, "An asset", typeof(Asset))]
 		public async Task<IActionResult> GetById(string id)
 		{
-			var asset = await _contentService.GetByIdAsync(id);
+			var asset = await _contentService.GetById(id);
 			if (asset == null)
 			{
 				return NotFound();
