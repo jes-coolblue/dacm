033798b8241de1b8dd420e772b4b5a349b78d11c
Jon Emil Sørensen <emil@cogoapp.io>
2024-07-03 01:29:31 +0200

    Fixed missing services

--- StartUp.cs
+++ StartUp.cs
@@ -1,47 +1,54 @@
 using Microsoft.OpenApi.Models;
+using DACM.Services;
+using DACM.Repositories;
+using DACM.Cache;
 
 namespace DACM
 {
 	public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
 
-            // Register the Swagger generator, defining one or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Content Distribution API", Version = "v1" });
-            });
+			// Register the Swagger generator, defining one or more Swagger documents
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Content Distribution API", Version = "v1" });
+			});
 
-            // Add memory cache
-            services.AddMemoryCache();
-        }
+			services.AddScoped<IAssetService, AssetService>();
+			services.AddScoped<ICacheService, CacheService>();
+			services.AddScoped<IRepository, Repository>();
+			
+			// Add memory cache
+			services.AddMemoryCache();
+		}
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                // Enable middleware to serve generated Swagger as a JSON endpoint
-                app.UseSwagger();
-                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Content Distribution API v1"));
-            }
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				// Enable middleware to serve generated Swagger as a JSON endpoint
+				app.UseSwagger();
+				// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Content Distribution API v1"));
+			}
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseHttpsRedirection();
+			app.UseRouting();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
